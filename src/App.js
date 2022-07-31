fd1eed0b1cbbc9989de215832ccea301ae855a94
bunder12 <fajriwork12@gmail.com>
2022-07-31 11:31:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import Select from 'react-select'
+import axios from 'axios';
+
+
+const App = () => {
+
+  const [data, setData] = useState([]);
+  const [val, setVal] = useState("");
+
+  const getCoba = async () => {
+    const value = await axios.get("https://pokeapi.co/api/v2/berry")
+    // const valueJson = await value.json()
+    console.log(value)
+    let res = value.data.results.map(data => {
+      return{
+        label: data.name,
+        value: data.name
+      }
+    })
+    setData(res.sort((a, b) => a.label.localeCompare(b.label)))
+  }
+
+  useEffect(() => {
+    getCoba()
+  },[])
+
+  const handleChange = (value) => {
+    setVal(value)
+  }
+
+  return (
+    <div>
+      <h1>{val}</h1>
+      <Select options={data} onChange={(e) => handleChange(e.value)}/>
+    </div>
+  )
+}
+
+export default App
